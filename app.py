3c2c8cbd73ce32691b08c2610f23845fbd44a43b
Brian Chaya <brian.chaya2005@gmail.com>
2026-04-01 11:19:28 +0700

    Update app.py

--- app.py
+++ app.py
@@ -272,7 +272,7 @@ def grouping(db):
     normal = grouped[grouped["TYPE"] == "NORMAL"]
     double = grouped[grouped["TYPE"] == "DOUBLE"]
 
-    db_na = db_na.drop_duplicates(subset=["Description"])
+    db_na = db_na.drop_duplicates(subset=["ID", "Description"])
 
     db_na["TYPE"] = "NA"
 
